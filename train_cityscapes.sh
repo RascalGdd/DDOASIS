0bdfa8f1abb6092e31c9959984df598acfc237e3
Mark Ashraf (Fouad) <h162@login1.lss.uni-stuttgart.de>
2022-03-08 11:38:27 +0100

    deleted info file, and updated sh

--- train_cityscapes.sh
+++ train_cityscapes.sh
@@ -1,6 +1,19 @@
-python train.py --name oasis_cityscapes_unsupervised_gen_8_bilinear_residual_nearest_nonresidual_wavelet_spade --dataset_mode cityscapes --gpu_ids 0 \
---dataroot /data/public/cityscapes --no_labelmix \
---batch_size 1 --model_supervision 0 --supervised_num 20 \
---Du_patch_size 64 --netDu wavelet  \
---netG 8 --channels_G 16 \
- --num_epochs 500
+#!/bin/bash -l
+ 
+# Slurm parameters
+#SBATCH --job-name=job_name
+#SBATCH --output=job_name%j.%N.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=6-23:00:00
+#SBATCH --mem=64G
+#SBATCH --gpus=1
+#SBATCH --qos=batch
+#SBATCH --nodelist=linse19
+
+python train.py --name semisupervised_S4U4 --dataset_mode cityscapes --gpu_ids 0 \
+--dataroot /data/public/cityscapes --batch_size 4  --no_labelmix \
+--Du_patch_size 64 --netDu wavelet --netG 0 \
+--channels_G 64 --num_epochs 1500 \
+--model_supervision 1 --supervised_percentage 50 \
+--results_dir /no_backups/h162/ 
